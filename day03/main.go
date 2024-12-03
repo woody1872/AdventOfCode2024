6b2efef9f9c7bc4e90c1058fa6e5067714d265b2
Sam Wood <samwooddev@gmail.com>
2024-12-03 11:58:16 +0000

    Day 3 part 2

--- day03/main.go
+++ day03/main.go
@@ -15,8 +15,8 @@ func main() {
 	}
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
 
+	// Part 1
 	re := regexp.MustCompile(`mul\(\d+,\d+\)`)
-
 	total := 0
 	for _, line := range lines {
 		matches := re.FindAllString(strings.TrimSpace(line), -1)
@@ -27,4 +27,26 @@ func main() {
 		}
 	}
 	fmt.Println("Answer (part 1):", total)
+
+	// Part 2
+	re2 := regexp.MustCompile(`mul\(\d+,\d+\)|do\(\)|don't\(\)`)
+	do := true
+	total = 0
+	for _, line := range lines {
+		matches := re2.FindAllString(strings.TrimSpace(line), -1)
+		for _, match := range matches {
+			if match == "do()" {
+				do = true
+			}
+			if match == "don't()" {
+				do = false
+			}
+			if do {
+				var n1, n2 int
+				fmt.Sscanf(match, "mul(%d,%d)", &n1, &n2)
+				total += (n1 * n2)
+			}
+		}
+	}
+	fmt.Println("Answer (part 2):", total)
 }
